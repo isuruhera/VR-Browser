b50153ef4b83f6057f6b55ce03ab25f82bc1595f
vso003 <31914688+vso003@users.noreply.github.com>
2017-10-16 20:05:00 +1300

    jQuery post request script.

---
+++ PostGuestbook.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="jquery-1.10.2.min.js" />
+$(document).ready(function () {
+    $('#btnAddQuestbookInfo').click(function () {
+        var guestbook = {};
+        guestbook.Name = $('#txtName').val();
+        guestbook.Url = $('#txtUrl').val();
+        guestbook.Category = $('#txtCategory').val();
+
+        $.ajax({
+            url: 'api/questbook',
+            method: 'post',
+            data: JSON.stringify(guestbook),
+            contentType: "application/json; charset=utf-8",
+            success: function () {
+                alert("Data is successfully saved!");
+            },
+            error: function (err) {
+                alert(err);
+            }
+        });
+    });
+});
